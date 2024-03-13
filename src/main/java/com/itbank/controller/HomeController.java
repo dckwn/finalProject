14fc42dd93748c239a052a86434f2345b7803381
dckwn <d_ckwn@nate.com>
2024-03-13 10:07:50 +0900

    testtest

--- src/main/java/com/itbank/controller/HomeController.java
+++ src/main/java/com/itbank/controller/HomeController.java
@@ -1,43 +1,13 @@
 package com.itbank.controller;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.itbank.component.CalenderComponent;
-import com.itbank.model.CalenderDTO;
-
 
 @Controller
 public class HomeController {
 
-	@Autowired private CalenderComponent cal;
-	
 	@GetMapping("/")
-	public ModelAndView home(@RequestParam(required=false) String strmonth) {
-		ModelAndView mav = new ModelAndView("/home");
-		CalenderDTO dto = new CalenderDTO();
-		
-		Date wantDate = new Date(new java.util.Date().getTime());
-
-        // SimpleDateFormat 객체를 사용하여 Date를 원하는 형식의 String으로 변환
-        SimpleDateFormat sdf = new SimpleDateFormat("MM");
-        int month = 0;
-        if(strmonth == null) {month = Integer.parseInt(sdf.format(wantDate));}	
-        else {month = Integer.parseInt(strmonth);}
-		
-		dto.setMonth(cal.getYoil(month));
-		dto.setLastDay(cal.getLastDay(month));
-		dto.setStartDayOfWeek(cal.getStartDayOfWeek(month));
-		
-		mav.addObject("cal", dto);
-		mav.addObject("month", month);
-		return mav;
+	public String home() {
+		return "home";
 	}
 }
